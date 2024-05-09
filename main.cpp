e1dc21547d6da85f2822755860e8662d275b3c5f
Ivan Davidović <hedead421@gmail.com>
2024-05-09 19:29:25 +0200

    Update

--- main.cpp
+++ main.cpp
@@ -2,47 +2,33 @@
 #include "rc.h"
 #include <filesystem>
 
-Gdiplus::Image* Image = nullptr;
-TCHAR imagepath[MAX_PATH] = { 0 };
-
-void file_open(HWND parent)
-{
-	TCHAR path[MAX_PATH]; *path = 0;
-	ZeroMemory(path, sizeof(path));
-	TCHAR filter[] = _T("Image\0*.png;*\0*.jpeg;*\0*.bmp;*\0*.gif;*\0*.tiff;*\0*.emf\0");
-	OPENFILENAME ofn{ sizeof OPENFILENAME };
-	ofn.hwndOwner = parent;
-	ofn.lpstrFile = path;
-	ofn.nMaxFile = MAX_PATH;
-	ofn.lpstrFilter = filter;
-	ofn.Flags = OFN_HIDEREADONLY;
-	if (::GetOpenFileName(&ofn)) {
-		Image = Gdiplus::Image::FromFile(path);
-		_tcscpy_s(imagepath, MAX_PATH, path);
-	}
-}
-
-void draw_string(Gdiplus::Graphics& g, const TCHAR* text, int x, int y) {
-	Gdiplus::Font font(L"Arial", 12);
-	Gdiplus::SolidBrush brush1(Gdiplus::Color(0, 0, 0));
-	Gdiplus::SolidBrush brush2(Gdiplus::Color(255, 255, 255));
-	Gdiplus::StringFormat format;
-	Gdiplus::Rect TR;
-	Gdiplus::PointF point1(x + 2, y + 2);
-	g.DrawString(text, -1, &font, point1, &brush1);
-	Gdiplus::PointF point2(x, y);
-	g.DrawString(text, -1, &font, point2, &brush2);
-}
-
 void main_window::on_paint(HDC hdc) 
 {
 	RECT rc;
 	GetClientRect(*this, &rc);
-	int widht = (rc.right - rc.left)/4;
-	int height = rc.bottom - rc.top;
 	Gdiplus::Graphics g(hdc);
-	g.DrawImage(Image,0,0, rc.right, rc.bottom);
-	draw_string(g,imagepath,widht,height-50);
+	g.DrawImage(Image.get(), 0, 0, rc.right, rc.bottom);
+
+	Gdiplus::Font font(L"Arial", 12);
+	//Gdiplus::SolidBrush brush1(Gdiplus::Color(0, 0, 0));
+	//Gdiplus::SolidBrush brush2(Gdiplus::Color(255, 255, 255));
+	Gdiplus::SolidBrush brush1(Gdiplus::Color::Black);
+	Gdiplus::SolidBrush brush2(Gdiplus::Color::White);
+	Gdiplus::StringFormat fmat;
+	Gdiplus::RectF TR(rc.left, rc.top, rc.right, rc.bottom);
+	fmat.SetAlignment(Gdiplus::StringAlignmentCenter);
+	fmat.SetLineAlignment(Gdiplus::StringAlignmentFar);
+
+	std::filesystem::path filePath(imagePath);
+	std::wstring fileName = filePath.filename();
+	
+	//int widht = (rc.right - rc.left) / 2.0f;
+	//int height = rc.bottom-70;
+
+	//Gdiplus::PointF point1(); //iskoristiti alignment
+	g.DrawString(fileName.c_str(), -1, &font, TR, &fmat, &brush1);
+	//Gdiplus::PointF point2();
+	g.DrawString(fileName.c_str(), -1, &font, TR, &fmat, &brush2);
 
 }
 
@@ -50,9 +36,27 @@ void main_window::on_command(int id)
 {
 	switch (id) 
 	{
-		case ID_OPEN:
-			file_open(*this);
+	case ID_OPEN: {
+
+		TCHAR path[MAX_PATH]; *path = 0;
+		ZeroMemory(path, sizeof(path));
+		TCHAR filter[] = _T("Image Files\0*.jpeg;*.jpg;*.png;*.bmp;*.gif;*.tif;*.tiff;*.emf\0");
+		OPENFILENAME ofn{ sizeof OPENFILENAME };
+		ofn.hwndOwner = *this;
+		ofn.lpstrFile = path;
+		ofn.nMaxFile = MAX_PATH;
+		ofn.lpstrFilter = filter;
+		ofn.Flags = OFN_HIDEREADONLY;
+		if (::GetOpenFileName(&ofn)) {
+			auto newImage = Gdiplus::Image::FromFile(path);
+			if (newImage->GetLastStatus() == Gdiplus::Ok)
+			{
+				imagePath = path;
+				Image.reset(newImage);
+			}
 			InvalidateRect(*this, NULL, true);
+		}
+	}
 			break;
 		case ID_EXIT:
 			DestroyWindow(*this);
